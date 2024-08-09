30cf12454f06b5e39709d5e1104753bdd92ce57c
Mounika <212687639@ge.com>
2024-08-09 16:30:07 +0530

    Hosting to Github pages

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-CTG5LCSV.mjs";import"./chunk-UOQRINLF.mjs";import"./chunk-KLLIM67V.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'next-hr';
+
+  videos = [];
+  games = [];
+  reads = [];
+  problems = [];
+  puzzles = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.videos = [
+      
+    ];
+  }
 }
